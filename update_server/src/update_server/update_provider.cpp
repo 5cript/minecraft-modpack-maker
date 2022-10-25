e3bb9587c581340109b408cac8f5f1606df174b5
5cript <tim06tr@gmail.com>
2022-10-25 04:48:44 +0200

    Starting minecraft.

--- update_server/src/update_server/update_provider.cpp
+++ update_server/src/update_server/update_provider.cpp
@@ -27,6 +27,7 @@ UpdateProvider::UpdateProvider(std::filesystem::path const& serverDirectory)
     , localMods_{}
 {
     loadLocalMods();
+    minecraft_.start(serverDirectory / "server.jar");
 }
 //---------------------------------------------------------------------------------------------------------------------
 void UpdateProvider::loadLocalMods()
